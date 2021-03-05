7161e2808d65fd9a1cbd8b7ce32e2d338f11f397
SkyLined <github@skylined.nl>
2021-03-05 09:51:18 +0100

    Allow decoding using arbitrary keys

--- Tests/Tests.py
+++ Tests/Tests.py
@@ -30,20 +30,39 @@ try:
       def fStatus(*txArguments, **dxArguments):
         pass;
   
-  import base64;
+  import base64, random, sys;
   from fsBase64Decode import fsBase64Decode;
+  
+  bDebug = "--debug" in sys.argv;
   
   for sData in [
     "Hello, world!",
     "".join([chr(u) for u in range(256)]),
   ]:
+    print "sData: %s" % repr(sData);
     sEncodedData = base64.b64encode(sData);
+    print "sEncodedData: %s" % repr(sEncodedData);
     assert base64.b64decode(sEncodedData) == sData, \
         "%s decodes to %s instead of %s" % (repr(sEncodedData), repr(sDecodedData), repr(sData));
-    sDecodedData = fsBase64Decode(sEncodedData, True);
+    sDecodedData = fsBase64Decode(sEncodedData, bDebug = bDebug);
+    print "sDecodedData: %s" % repr(sDecodedData);
     assert sDecodedData == sData, \
         "%s decodes to %s instead of %s" % (repr(sEncodedData), repr(sDecodedData), repr(sData));
-  
+    
+    sNormalKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    asRandomChars = [chr(u) for u in xrange(0x21, 0x7E)];
+    random.shuffle(asRandomChars);
+    sRandomKey = "".join(asRandomChars)[:65];
+    print "sRandomKey: %s" % repr(sRandomKey);
+    sRandomEncodedData = "".join([
+      sRandomKey[sNormalKey.find(sChar)]
+      for sChar in sEncodedData
+    ]);
+    print "sRandomEncodedData: %s" % repr(sRandomEncodedData);
+    sDecodedData = fsBase64Decode(sRandomEncodedData, s0Key = sRandomKey, bDebug = bDebug);
+    print "sDecodedData: %s" % repr(sDecodedData);
+    assert sDecodedData == sData, \
+        "%s\ndecodes to \n%s\ninstead of\n%s" % (repr(sEncodedData), repr(sDecodedData), repr(sData));
 except Exception as oException:
   if mDebugOutput:
     mDebugOutput.fTerminateWithException(oException, bShowStacksForAllThread = True);
--- fsBase64Decode.py
+++ fsBase64Decode.py
@@ -1,4 +1,24 @@
-def fsBase64Decode(sBase64EncodedData, bDebug = False):
+import re;
+
+def fsShowBits(uValue, uNumberOfBits):
+  if uNumberOfBits == 0:
+    return "";
+  sBits = bin(uValue)[2:].rjust(uNumberOfBits, "0");
+  return re.sub(r"(.{8})(?=.)", r"\1 ", sBits); # Add spaces between bytes
+
+gsWhiteSpaceChars = " \t\r\n";
+gsDefaultKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+
+def fsBase64Decode(sBase64EncodedData, s0Key = None, bDebug = False):
+  sKey = s0Key if s0Key is not None else gsDefaultKey;
+  assert len(sKey) == 65, \
+      "Base 64 decoding requires a 65 character key, not %d!" % len(sKey);
+  for sWhitespaceChar in gsWhiteSpaceChars:
+    assert sWhitespaceChar not in sKey, \
+        "Whitespace character %s (%02X) must not be used in the key!" % \
+        (repr(sWhitespaceChar), ord(sWhitespaceChar));
+  sIndexTable = sKey[:64];
+  sPadChar = sKey[64];
   uBitCache = 0;
   uBitsInBitCache = 0;
   sDecodedData = "";
@@ -6,56 +26,90 @@ def fsBase64Decode(sBase64EncodedData, bDebug = False):
   uPaddingChars = 0;
   for uIndex in xrange(len(sBase64EncodedData)):
     sBase64Char = sBase64EncodedData[uIndex];
-    if sBase64Char in " \t\r\n":
+    if sBase64Char in gsWhiteSpaceChars:
       continue;
-    if sBase64Char == "=":
+    if sBase64Char == sPadChar:
       if uBitsInBitCache == 0:
         print "Base64 data has a superfluous padding character at offset %d: assuming end of base64 data." % uIndex;
         break;
       uBase64Chars += 1;
       uPaddingChars += 1;
+      u6Bits = 0;
       uBitCache <<= 6;
       uBitsInBitCache -= 2;
+      if bDebug:
+        print "PAD:    %6s (%02X -> %s)   | cache: %-16s %2d bits" % (
+                       repr(sBase64Char),
+                            ord(sBase64Char),
+                                    fsShowBits(u6Bits, 6),
+                                                fsShowBits(uBitCache, uBitsInBitCache),
+                                                       uBitsInBitCache
+        ),;
+        assert uBitCache == 0 or len(bin(uBitCache)[2:]) <= uBitsInBitCache, \
+          "Expected %d bits, got %s" % (uBitsInBitCache, bin(uBitCache)[2:]);
     elif uPaddingChars == 0:
       uBase64Chars += 1;
-      u6Bits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".find(sBase64Char);
+      u6Bits = sIndexTable.find(sBase64Char);
       if u6Bits == -1:
         print "Base64 data has an invalid character at offset %d: assuming end of base64 data." % uIndex;
         break;
       uBitCache = (uBitCache << 6) + u6Bits;
       uBitsInBitCache += 6;
+      if bDebug:
+        print "DATA:   %6s (%02X -> %s)   | cache: %-16s %2d bits" % (
+                       repr(sBase64Char),
+                            ord(sBase64Char),
+                                    fsShowBits(u6Bits, 6),
+                                                fsShowBits(uBitCache, uBitsInBitCache),
+                                                       uBitsInBitCache
+        ),;
+        assert uBitCache == 0 or len(bin(uBitCache)[2:]) <= uBitsInBitCache, \
+          "Expected %d bits, got %s" % (uBitsInBitCache, bin(uBitCache)[2:]);
     else:
       print "Base64 data has a non-padding characters following a padding character at offset %d: assuming end of base64 data." % uIndex;
       break;
-    if bDebug:
-      print "base64: %s (%02X), cache: %s (%d bits)" % (
-        repr(sBase64Char), ord(sBase64Char),
-        bin(uBitCache)[2:].rjust(uBitsInBitCache, "0"), uBitsInBitCache
-      );
     if uBitsInBitCache >= 8:
+#      print "uBitCache = 0x%X (%d bits)" % (uBitCache, uBitsInBitCache);
       uBitsInBitCache = uBitsInBitCache - 8;
+#      print "uByte = 0x%X" % (uBitCache >> uBitsInBitCache);
       uByte = uBitCache >> uBitsInBitCache;
-      sDecodedData += chr(uByte);
       uBitCache -= uByte << uBitsInBitCache;
       if bDebug:
-        print "  byte: %s (%02X), cache: %s (%d bits)" % (
-          repr(chr(uByte)), uByte,
-          bin(uBitCache)[2:].rjust(uBitsInBitCache, "0"), uBitsInBitCache
-        );
+        print "| OUT: %s (%02X)" % (repr(chr(uByte)), uByte),
+      sDecodedData += chr(uByte);
       if uPaddingChars > 1 and (len(sDecodedData) % 3) + uPaddingChars == 3:
         break;
+    if bDebug:
+      print;
+  if bDebug:
+    print;
   uMissingPaddingChars = 0;
   while uBitsInBitCache != 0:
     uMissingPaddingChars += 1;
     uBitCache <<= 6;
     uBitsInBitCache += 6;
+    if bDebug:
+      print "MISSING:       (00 -> 000000)   | cache: %16s (%d bits)" % (
+                                                    fsShowBits(uBitCache, uBitsInBitCache),
+                                                          uBitsInBitCache
+      );
     uBitsInBitCache = uBitsInBitCache - 8;
     uByte = uBitCache >> uBitsInBitCache;
+    uBitCache -= uByte << uBitsInBitCache;
+    if bDebug:
+      print "  byte: %02X (%s), cache: %s (%d bits)" % (
+        uByte, bin(uByte)[2:].rjust(8, "0"),
+        re.sub(
+        r"(.{8})",
+        r"\1 ",
+        bin(uBitCache)[2:].rjust(uBitsInBitCache, "0")
+      ),
+      uBitsInBitCache
+      );
     sDecodedData += chr(uByte);
-    uBitCache -= uBitCache << uBitsInBitCache;
   if uMissingPaddingChars != 0:
     print "Base64 data is missing %d padding character%s at offset %d" % \
-        (uMissingPaddingChars, "s" if uMissingPaddingChars == 1 else "");
+        (uMissingPaddingChars, "s" if uMissingPaddingChars == 1 else "", uIndex);
   return sDecodedData;
 
 if __name__ == "__main__":
@@ -63,12 +117,17 @@ if __name__ == "__main__":
   sInputFilePath = None;
   sOutputFilePath = None;
   bDebug = False;
+  s0Key = None;
   for sArgument in sys.argv[1:]:
     if sArgument in ["--help", "-h", "-?", "/help", "/h", "/?"]:
-      print "Usage: fsBase64Decode [input file] [output file] [--debug]";
+      print "Usage: fsBase64Decode [input file] [output file] [--debug] [--key[=<65 chars>]]";
       sys.exit(0);
     elif sArgument == "--debug":
       bDebug = True;
+    elif sArgument == "--key":
+      s0Key = "";
+    elif sArgument.startswith == "--key=":
+      s0Key = sArgument[len("--key="):];
     elif sInputFilePath is None:
       sInputFilePath = sArgument;
     elif sOutputFilePath is None:
@@ -92,7 +151,12 @@ if __name__ == "__main__":
       if sInput == "": break;
       sBase64EncodedData += ("\n" if sBase64EncodedData else "") + sInput;
   
-  sDecodedData = fsBase64Decode(sBase64EncodedData, len(sys.argv) > 3 and sys.argv[3] == "--debug");
+  if s0Key == "":
+    print "Please enter 65 character key (64 data + 1 padding):";
+    s0Key = raw_input("");
+  assert s0Key is None or len(s0Key) == 65, \
+      "Key must be 65 characters, not %d" % len(s0Key);
+  sDecodedData = fsBase64Decode(sBase64EncodedData, s0Key = s0Key, bDebug = bDebug);
   
   if sOutputFilePath:
     try:
